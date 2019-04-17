fde52f8152176d85bdfcf6bfe8c878770bd2cfde
Vladilav Yepifanov <evn1999@yandex.ru>
2019-04-17 17:08:08 +0300

    define lock public stuff

--- src/main/java/com/example/latch/CountDownLatch.java
+++ src/main/java/com/example/latch/CountDownLatch.java
@@ -1,4 +1,32 @@
 package com.example.latch;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class CountDownLatch {
+    final Condition isZero;
+    final Condition notZero;
+
+    final Lock modifyValue;
+
+    volatile int value;
+
+    public CountDownLatch(int startingValue) {
+        modifyValue = new ReentrantLock();
+        isZero = new ReentrantLock().newCondition();
+        notZero = new ReentrantLock().newCondition();
+    }
+
+    public void await() {
+
+    }
+
+    public void countDown() {
+
+    }
+
+    public void countUp() {
+
+    }
 }
